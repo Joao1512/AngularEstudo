945e64346a7416bfea63cf057b8a7443ca0f32fa
Joao1512 <jvictorsimao@hotmail.com>
2020-03-13 17:20:17 -0300

    gerando os componentes

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CursosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
