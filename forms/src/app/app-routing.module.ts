70126335f552e767a120f022a43dd774724846e3
Joao1512 <jvictorsimao@hotmail.com>
2020-03-20 18:04:01 -0300

    iniciando o projeto com bootstrap

--- forms/src/app/app-routing.module.ts
+++ forms/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { DataFormComponent } from './components/data-form/data-form.component';
+import { TemplateFormComponent } from './components/template-form/template-form.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Router } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'templateForm', component: TemplateFormComponent },
+  { path: 'dataForm', component: DataFormComponent },
+  { path: '', component: TemplateFormComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
